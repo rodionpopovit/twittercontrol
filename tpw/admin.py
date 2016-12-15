1e36982860609ee008ba6e5ab68068256db90ac8
Rodion Popov <rodionpopovit@gmail.com>
2016-12-15 14:29:05 +0200

    remove everything

--- tpw/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import TweetMsg, Community, TriggerFrequency, TriggerRule
-
-# Register your models here.
-admin.site.register(Community)
-admin.site.register(TriggerFrequency)
-admin.site.register(TriggerRule)
-admin.site.register(TweetMsg)
